fc547b76afe42df197101212b3fca15219657ce9
falling <zyz635857938@gmail.com>
2017-03-25 20:34:35 +0800

    Goods

--- app/src/main/java/com/ycl/tabview/Activity/GoodsActivity.java
+++ app/src/main/java/com/ycl/tabview/Activity/GoodsActivity.java
@@ -2,6 +2,7 @@ package com.ycl.tabview.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import com.ycl.tabview.R;
 
@@ -10,9 +11,18 @@ import com.ycl.tabview.R;
  */
 
 public class GoodsActivity extends Activity {
+
+    private TabHost mTabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goods_activity);
+        mTabHost = (TabHost) findViewById(R.id.tabHost);
+        mTabHost.setup();// init
+        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("考试详情").setContent(R.id.tab1));
+        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("发布者信息").setContent(R.id.tab2));
+        mTabHost.addTab(mTabHost.newTabSpec("tab3").setIndicator("留言").setContent(R.id.tab3));
+
     }
 }
