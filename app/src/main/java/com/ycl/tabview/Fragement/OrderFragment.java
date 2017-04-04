88cf74ead0bf47bffa1b086bb946755b23c32e2c
falling <zyz635857938@gmail.com>
2017-04-04 17:09:52 +0800

    date

--- app/src/main/java/com/ycl/tabview/Fragement/OrderFragment.java
+++ app/src/main/java/com/ycl/tabview/Fragement/OrderFragment.java
@@ -1,6 +1,7 @@
 package com.ycl.tabview.Fragement;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,7 +9,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
@@ -26,130 +26,83 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
-import io.reactivex.Observer;
+import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.Subject;
 
 /**
  * Created by Administrator on 2017/3/18.
  */
 
-public class OrderFragment extends Fragment implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener, WeekView.EmptyViewLongPressListener{
+public class OrderFragment extends Fragment implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener, WeekView.EmptyViewLongPressListener {
 
-    private static OrderFragment orderFragment =new OrderFragment();
+    private static OrderFragment orderFragment = new OrderFragment();
     private WeekView mWeekView;
+    private int ranColor;
     private Myapplication mMyapplication;
     private List<Exam> mData = new ArrayList<>();
-    public static OrderFragment newInstance(){
+    private List<WeekViewEvent> mEvents = new ArrayList<>();
+
+    public static OrderFragment newInstance() {
         return orderFragment;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.order_fragment,container,false);
+        View view = inflater.inflate(R.layout.order_fragment, container, false);
         // Get a reference for the week view in the layout.
         mWeekView = (WeekView) view.findViewById(R.id.weekView);
         mMyapplication = (Myapplication) getActivity().getApplication();
 
         mWeekView.setOnEventClickListener(this);
 
-// The week view has infinite scrolling horizontally. We have to provide the events of a
-// month every time the month changes on the week view.
         mWeekView.setMonthChangeListener(this);
-
-// Set long press listener for events.
-//        mWeekView.setEventLongPressListener(mEventLongPressListener);
-        return view;
-    }
-
-    @Override
-    public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-        WeekViewEvent event;
-
 
         Retrofitutil.getmRetrofit().create(LoginHttps.class).completeJson(mMyapplication.users.getUser_id())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subject<ExamBean>() {
+                .subscribe(new Consumer<ExamBean>() {
                     @Override
-                    public boolean hasObservers() {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean hasThrowable() {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean hasComplete() {
-                        return false;
-                    }
-
-                    @Override
-                    public Throwable getThrowable() {
-                        return null;
-                    }
-
-                    @Override
-                    protected void subscribeActual(Observer<? super ExamBean> observer) {
-
-                    }
-
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(ExamBean s) {
+                    public void accept(@NonNull ExamBean s) throws Exception {
                         mData.clear();
                         mData.addAll(s.getList());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onComplete() {
+                        mEvents.clear();
+                        for (int i = 0; i < mData.size(); i++) {
+                            // 随机颜色
+                            Random random = new Random();
+                            ranColor = 0xff000000 | random.nextInt(0x00ffffff);
+                            Calendar startTime = Calendar.getInstance();
+                            startTime.set(Calendar.HOUR_OF_DAY, Integer.valueOf(mData.get(i).getExam_time().split(":")[0]));
+                            startTime.set(Calendar.MINUTE, Integer.valueOf(mData.get(i).getExam_time().split(":")[1]));
+                            startTime.set(Calendar.DATE, Integer.valueOf(mData.get(i).getExam_date().split("-")[2]));
+                            startTime.set(Calendar.MONTH, Integer.valueOf(mData.get(i).getExam_date().split("-")[1])-1);
+                            startTime.set(Calendar.YEAR, Integer.valueOf(mData.get(i).getExam_date().split("-")[0]));
+                            Calendar endTime = (Calendar) startTime.clone();
+                            endTime.set(Calendar.HOUR_OF_DAY, Integer.valueOf(mData.get(i).getExam_endtime().split(":")[0]));
+                            endTime.set(Calendar.MINUTE, Integer.valueOf(mData.get(i).getExam_endtime().split(":")[1]));
+                            WeekViewEvent event = new WeekViewEvent(i, getEventTitle(startTime), startTime, endTime);
+                            event.setColor(ranColor);
+                            mEvents.add(event);
+                        }
+                        mWeekView.notifyDatasetChanged();
                     }
                 });
-        for(int i=0;i<mData.size();i++){
-            // 随机颜色
-            Random random = new Random();
-            int ranColor = 0xff000000 | random.nextInt(0x00ffffff);
+        return view;
+    }
 
+    @Override
+    public List<? extends WeekViewEvent> onMonthChange(final int newYear, final int newMonth) {
+        final List<WeekViewEvent> events = new ArrayList<>();
+        for (WeekViewEvent event : mEvents) {
+            if (event.getStartTime().get(Calendar.MONTH) +1 == newMonth
+                    && event.getStartTime().get(Calendar.YEAR) == newYear){
+                events.add(event);
+            }
         }
-
-        Calendar startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 5);
-        startTime.set(Calendar.MINUTE, 30);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        Calendar endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 2);
-        endTime.set(Calendar.MONTH, newMonth-1);
-        event = new WeekViewEvent(2, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.colorPrimary));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 4);
-        startTime.set(Calendar.MINUTE, 20);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.set(Calendar.HOUR_OF_DAY, 5);
-        endTime.set(Calendar.MINUTE, 0);
-        event = new WeekViewEvent(10, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.colorAccent));
-        events.add(event);
         return events;
     }
 
@@ -157,7 +110,6 @@ public class OrderFragment extends Fragment implements WeekView.EventClickListen
     public void onEmptyViewLongPress(Calendar time) {
 
     }
-
 
 
     @Override
@@ -173,6 +125,6 @@ public class OrderFragment extends Fragment implements WeekView.EventClickListen
     }
 
     protected String getEventTitle(Calendar time) {
-        return String.format("Event of %02d:%02d %s/%d", time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE), time.get(Calendar.MONTH)+1, time.get(Calendar.DAY_OF_MONTH));
+        return String.format("Event of %02d:%02d %s/%d", time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE), time.get(Calendar.MONTH) + 1, time.get(Calendar.DAY_OF_MONTH));
     }
 }
